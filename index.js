94543db7006c8a6b008b0ecf02d0bd5699b4c761
Joshua Johnson <jwj1285@gmail.com>
2021-03-01 20:32:54 -0800

    created server and setup connections

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const connectionString = process.env.MONDBURI || 'mongodb://localhost:27017/everdecision';
+
+mongoose.connect(connectionString, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB Connected Successfully');
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB was Disconnected');
+});
+
+mongoose.connection.on('err', (err) => {
+  console.log(err);
+});
+
+// module.exports = {
+//   User: require('./User'),
+//   Post: require('./Post'),
+//   Comment: require('./Comment'),
+//   Class: require('./Class'),
+// }
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use('/')
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
+});
